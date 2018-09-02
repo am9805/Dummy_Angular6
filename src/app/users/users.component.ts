b981492b77a1f9f2d64cb4f4cbd85bd08920867b
Angélica <angelica.arroyavem@udea.edu.co>
2018-09-01 22:24:29 -0500

    Now, when the person´s name is clicked, we are gonna direct to the details page; the Id came of the API. Again :v

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService} from '../data.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users$: Object;
 
+  constructor(private data: DataService) { }
+
+  //Es uno de los lifecycle hooks de angular para los componentes
+  //Cualquier comando ejecutado aqui, va a ser ejecutado cuando este componente
+  //cargue
   ngOnInit() {
+    this.data.getUsers().subscribe(
+      data => this.users$ = data
+    )
   }
 
 }
